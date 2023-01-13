09cc5d1f82739421b0e961f74189ce56ea906284
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2023-01-13 13:09:03 +0200

    Update ServerThread.java

--- ServerThread.java
+++ ServerThread.java
@@ -4,17 +4,18 @@ import java.net.*;
 class ServerThread extends Thread
 {
 	private Socket myDataSocket;
-        private Socket otherDataSocket;
+    private Socket otherDataSocket;
 	private InputStream is;
    	private BufferedReader in;
 	private OutputStream os;
    	private PrintWriter out;
 	private static final String EXIT = "CLOSE";
 
+	
    	public ServerThread(Socket socket1, Socket socket2)
    	{
       		myDataSocket = socket1;
-                otherDataSocket = socket2;
+            otherDataSocket = socket2;
       		try {
 			is = myDataSocket.getInputStream();
 			in = new BufferedReader(new InputStreamReader(is));
@@ -30,6 +31,8 @@ class ServerThread extends Thread
 	{
    		String inmsg, outmsg;
 		
+   		// server wait for the message
+   		// and send it back to the client
 		try {
 			inmsg = in.readLine();
 			ServerProtocol app = new ServerProtocol();
