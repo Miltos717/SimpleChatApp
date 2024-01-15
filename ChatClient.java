2daeaffb8c6691a8e3df40a783a2b9d54803549c
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2024-01-15 10:23:18 +0200

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -10,6 +10,7 @@ public class ChatClient {
     //private static final InetAddress HOST = InetAddress.getLocalHost();
     private static final String HOST = "localhost";
 	// new violation in line 12
+	// violation moves to 13 from 12
 	System.out.println("Server is waiting first client in port: " + PORT);
     	// we create and start one thread for the receiving messages and
     	// one for the sending messages
