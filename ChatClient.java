98370d0b0c32d7a6476a470e31aedff208b9217e
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2024-11-05 09:37:46 +0200

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -2,7 +2,7 @@
  import java.io.*;
   
  public class ChatClient {
-	
+	// ewewewe
 	private static final int PORT = 1234;
 	//  private static final InetAddress HOST = InetAddress.getLocalHost(); 
 	private static final String HOST = "localhost";
