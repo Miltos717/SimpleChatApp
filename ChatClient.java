b70a04b37b1b57ed3b3af557c94e92c3c075c12c
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2026-01-20 13:21:14 +0200

    Fix duplicate HOST variable declaration

--- ChatClient.java
+++ ChatClient.java
@@ -1,7 +1,7 @@
  import java.net.*;
  import java.io.*;
  
- public class ChatClient {
+ public class ChatClient { 
 	private static final int PORT = 1234;
 	private static final InetAddress HOST = InetAddress.getLocalHost(); 
 	private static final String HOST = "localhost";
